650cf6781d6cf0181728c34c6e7468174143d669
Jonathan Ng <jonngkh@hotmail.com>
2015-07-06 16:17:43 +0100

    first test passes

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,12 @@
 describe('Thermostat', function() {
 
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees celsius', function() {
+    expect(thermostat.temperature()).toEqual(20);
+  });
 });
