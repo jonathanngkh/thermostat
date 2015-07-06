d779013e581f4bf3ed7afbcaeba6453fe451a643
Jonathan Ng <jonngkh@hotmail.com>
2015-07-06 18:03:51 +0100

    temperature can be reset

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -20,5 +20,43 @@ describe('Thermostat', function() {
       thermostat.down();
       expect(thermostat.show()).toEqual(19);
     });
+
+    it('not possible below 10', function() {
+      for (counter = 20; counter > 0; counter--) {
+        thermostat.down();
+      };
+      expect(thermostat.show()).toEqual(10);
+    });
   });
+
+  describe('powersaving mode', function() {
+    it('is on powersaving mode by default', function() {
+      expect(thermostat.powerSavingMode).toEqual(true);
+    });
+
+    it('can be switched off', function() {
+      thermostat.switchMode();
+      expect(thermostat.powerSavingMode).toEqual(false);
+    });
+
+    it('limits max temp by 25 when on', function() {
+      thermostat.temperature = 25;
+      thermostat.up();
+      expect(thermostat.show()).toEqual(25);
+    });
+
+    it('has a maximum temp of 32 when off', function() {
+      thermostat.switchMode();
+      thermostat.temperature = 32;
+      thermostat.up();
+      expect(thermostat.show()).toEqual(32);
+    });
+  });
+
+  it('can be reset', function() {
+    thermostat.temperature = 32;
+    thermostat.resetTemperature();
+    expect(thermostat.show()).toEqual(20);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,6 @@
 var Thermostat = function() {
-  this.temperature = 20
+  this.temperature = 20;
+  this.powerSavingMode = true;
 };
 
 Thermostat.prototype.show = function() {
@@ -7,9 +8,24 @@ Thermostat.prototype.show = function() {
 };
 
 Thermostat.prototype.up = function() {
-  this.temperature += 1
+  if (this.powerSavingMode && this.temperature < 25) {
+    this.temperature += 1;
+  };
+  if (!this.powerSavingMode && this.temperature < 32) {
+    this.temperature += 1;
+  };
 };
 
 Thermostat.prototype.down = function() {
-  this.temperature -= 1
+  if (this.temperature > 10) {
+    this.temperature -= 1;
+  };
+};
+
+Thermostat.prototype.switchMode = function() {
+  this.powerSavingMode = !this.powerSavingMode
+};
+
+Thermostat.prototype.resetTemperature = function() {
+  this.temperature = 20;
 };
